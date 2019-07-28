fixed some bugs for dice backprop; stil probs with nearest neigbor interpolatin and gradient

src/LossFunctions.py
@@ -41,20 +41,24 @@ class LossFunctions():
     
     denominator = 0.0
     numerator = 0.0
-    for label in uniqueVals[1:]:
-      trueSmooth = torch.zeros_like(y_true)
-      trueSmooth[y_true == label ] = 1.0
-      
-      predSmooth = torch.zeros_like(y_pred)
-      predSmooth[y_pred == label] = y_pred[y_pred == label]
-      predSmooth[predSmooth > 0] = 1.0
 
-      intersection = torch.sum(trueSmooth * predSmooth, dtype=torch.float32)
-      labelSum = torch.sum(trueSmooth, dtype=torch.float32) + torch.sum(predSmooth, dtype=torch.float32)
+    for label in uniqueVals[1:]:
+      trueSmooth = torch.zeros_like(y_true) - 1.0
+      trueSmooth[y_true == label ] = label
+      
+#       predSmooth = torch.zeros_like(y_pred)
+#       predSmooth[y_pred == label] = y_pred[y_pred == label]
+#       predSmooth[predSmooth > 0] = 1.0
+
+      intersection = torch.sum(y_pred[y_pred == trueSmooth], dtype=torch.float32) / label
+      
+#       intersection = torch.sum(trueSmooth * predSmooth, dtype=torch.float32)
+      labelSum = (torch.sum(y_pred[y_pred == label], dtype=torch.float32) + torch.sum(y_true[y_true == label], dtype=torch.float32)) / label
       denominator = denominator + labelSum
       numerator = numerator + intersection
       
     dice = 2. * numerator / (denominator + smooth)
+
     loss =  1 - dice
     if multiScale:
       for gaussKernel in self.gaussSmothingKernels:
@@ -64,18 +68,16 @@ class LossFunctions():
           if self.diceKernelMapping.has_key(gaussKernel) and self.diceKernelMapping[gaussKernel].has_key(label):
             trueSmooth = self.diceKernelMapping[gaussKernel][label]
           else:
-            trueSmooth = gaussKernel(y_true==label)
+            trueSmooth = gaussKernel(y_true * (y_true==label).to(torch.float32))
             if self.diceKernelMapping.has_key(gaussKernel):
               self.diceKernelMapping[gaussKernel][label] = trueSmooth
             else:
               self.diceKernelMapping[gaussKernel] = {label: trueSmooth}
               
-          predSmooth = torch.zeros_like(y_pred)
-          predSmooth[y_pred == label] = y_pred[y_pred == label]
-          predSmooth[predSmooth > 0] = 1.0              
+          predSmooth = y_pred * (y_pred == label).to(torch.float32)
           predSmooth = gaussKernel(predSmooth)          
-          intersection = torch.sum(trueSmooth * predSmooth, dtype=torch.float32)
-          labelSum = torch.sum(trueSmooth, dtype=torch.float32) + torch.sum(predSmooth,dtype=torch.float32)
+          intersection = torch.sum(trueSmooth * predSmooth, dtype=torch.float32) / label
+          labelSum = torch.sum(trueSmooth, dtype=torch.float32) + torch.sum(predSmooth,dtype=torch.float32) / label
           denominator = denominator + labelSum
           numerator = numerator + intersection
         dice = 2. * numerator / (denominator + smooth)

src/NetOptimizer.py
@@ -78,8 +78,8 @@ class NetOptimizer(object):
     return cycleImgData
 
 
-  def deformImage(self,imgDataToWork, addedField, nearestNeighbor = False):
-    imgDataDef = Utils.getImgDataDef(imgDataToWork.shape, self.userOpts.device, imgDataToWork.dtype)#torch.empty(imgDataToWork.shape, device=self.userOpts.device, requires_grad=False)#
+  def deformImage(self,imgDataToWork, addedField, nearestNeighbor = False, imgIdx=0):
+    imgDataDef = Utils.getImgDataDef(imgDataToWork.shape, self.userOpts.device, imgDataToWork.dtype, imgIdx)#torch.empty(imgDataToWork.shape, device=self.userOpts.device, requires_grad=False)#
     for chanIdx in range(-1, imgDataToWork.shape[1] - 1):
       imgToDef = imgDataToWork[:, None, chanIdx, ]
       chanRange = range(chanIdx * 3, chanIdx * 3 + 3)
@@ -144,7 +144,7 @@ class NetOptimizer(object):
     
     smoothnessLoss = lossCalculator.smoothnessVecField(self.userOpts.device)
     smoothnessDF = smoothnessLoss + boundaryLoss * self.userOpts.boundarySmoothnessW[itIdx]
-    
+
     if self.userOpts.diffeomorphicRegistration:
       deformationField = self.scalingSquaring(addedField)
     else:
@@ -159,12 +159,15 @@ class NetOptimizer(object):
     diceLoss = 0.0
     if labelToWork is not None:
       labelToWork = labelToWork[:,:,cropStart0:cropStart0+vecFields.shape[2], cropStart1:cropStart1+vecFields.shape[3], cropStart2:cropStart2+vecFields.shape[4]]
-      labelToWorkDef = self.deformImage(labelToWork, deformationField, True)
+      deformationField.register_hook(Utils.save_grad('deformationField'))
+      labelToWorkDef = self.deformImage(labelToWork, deformationField, False, 1)
+      labelToWorkDef.register_hook(Utils.save_grad('labelToWorkDef'))
       diceLoss = lossCalculator.multiLabelDiceLoss(labelToWork, labelToWorkDef, False)
-    
-    loss = crossCorrWeight * crossCorr + dscWeight * diceLoss + smoothNessWeight * smoothnessDF + self.userOpts.cycleW * cycleLoss    
-#     loss = dscWeight * diceLoss
-    if printLoss:
+
+    #loss = crossCorrWeight * crossCorr + dscWeight * diceLoss + smoothNessWeight * smoothnessDF + self.userOpts.cycleW * cycleLoss    
+    loss = dscWeight * diceLoss
+    #if printLoss:
+    if True:
       print('%.5f; %.5f; %5f; %5f; %.5f' % (loss, crossCorr, smoothnessDF, cycleLoss, diceLoss))
     torch.cuda.empty_cache()
           

src/Utils.py
@@ -76,14 +76,14 @@ def getZeroIdxField(imagShape, device):
   [idxs0, idxs1, idxs2, idxs3, idxs4] = Context.zeroIndices
   return [idxs0.clone(), idxs1.clone(), idxs2.clone(), idxs3.clone(), idxs4.clone()]
 
-def getImgDataDef(imagShape, device, dataType=torch.float32):
+def getImgDataDef(imagShape, device, dataType=torch.float32, imgIdx=0):
   if useropts.useContext:
-    if (dataType not in Context.imgDataDef) or (imagShape != Context.imgDataDef[dataType].shape):
+    if (dataType not in Context.imgDataDef) or (imagShape != Context.imgDataDef[imgIdx].shape):
       imgDataDef = torch.empty(imagShape, device=device, dtype=dataType, requires_grad=False)
-      Context.imgDataDef[dataType] = imgDataDef
+      Context.imgDataDef[imgIdx] = imgDataDef
     else:
-      Context.imgDataDef[dataType].detach_()
-    return Context.imgDataDef[dataType]
+      Context.imgDataDef[imgIdx].detach_()
+    return Context.imgDataDef[imgIdx]
   else:
     return torch.empty(imagShape, device=device, dtype=dataType, requires_grad=False)
 
@@ -270,10 +270,12 @@ def deformImage(imgToDef, defFields, device, detach=True, NN=False):
     currDefField[..., 0] = zeroDefField[..., 0] + defFields[:, 0, ] / ((imgToDef.shape[4]-1) / 2.0)
     currDefField[..., 1] = zeroDefField[..., 1] + defFields[:, 1, ] / ((imgToDef.shape[3]-1) / 2.0)
     currDefField[..., 2] = zeroDefField[..., 2] + defFields[:, 2, ] / ((imgToDef.shape[2]-1) / 2.0)
+  currDefField.register_hook(save_grad('currDefField'))
   if NN:
     deformedTmp = torch.nn.functional.grid_sample(imgToDef, currDefField, mode='nearest', padding_mode='border')
   else:
     deformedTmp = torch.nn.functional.grid_sample(imgToDef, currDefField, mode='bilinear', padding_mode='border')
+  deformedTmp.register_hook(save_grad('deformedTmp'))
   return deformedTmp
 
 def getReceptiveFieldOffset(nuOfLayers):